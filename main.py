3124da80112a39de1fd869a5fb7a480e39ea9f9b
Surakshya Banjade <myphoto7777777@gmail.com>
2024-07-05 17:03:24 +0545

    code changed

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-Hello... this is my first python project
+#initialization of git
+
+git init
+
+git status
+
+git add .
+
+git commit -m "commit message"
+
+git pull origin master
+
+git push origin master
+
+# to check the branch
+
+git branch
+
+git checkout branchName
+
