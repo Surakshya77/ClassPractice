0972e70107f37700faa9038004be32bec2f1add1
Surakshya Banjade <myphoto7777777@gmail.com>
2024-07-07 16:55:20 +0545

    Code revised for python class day 1

---
+++ basic of python/day1.py
@@ -0,0 +1,58 @@
+#print
+# print(1)
+# print("Hello World")
+# a=10
+# b=5
+# print(a)
+
+# define user variable called as name
+# name="Surakshya"
+# print(name)
+# print("Name is : ",name)
+
+# print("firstLine\n\n\n")
+# print("second Line")
+
+#Operators
+#Operator is a symbol to carry out arithmetical calculation or logical operations
+
+a=5
+b=3
+c=4
+sum=a + b + c
+print("Sum is :",sum)
+
+#Arithmetic operators ---> +,-,*,/,%
+
+#concatention of strings
+first_name="Surakshya"
+last_name="Banjade"
+name=first_name +' '+last_name
+print("Full name is:",name)
+
+#-->*
+result=2**2
+print("Multiplication result is:",result)
+
+#-->* can be used in repeatation of string
+a="python_automation"
+b=a*4
+print(b)
+
+#but it cannot be used as:
+a="abc"
+b=a*3
+print(b)
+
+#.../ and //
+result=17/2
+print("Result is :",result)
+
+# % operator
+result=9%2
+print("Result is :",result) # used for calculating the remainder point operations
+
+
+
+
+
