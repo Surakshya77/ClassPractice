5e33f0b68edb2c82bb9d807e09c5dc4cc1b41377
Surakshya Banjade <myphoto7777777@gmail.com>
2024-07-25 21:23:08 +0545

    edited simple login page with parameterized login concept through pom concept

--- pom/page/login_page.py
+++ pom/page/login_page.py
@@ -1,5 +1,29 @@
-# login page
+# simple login page
+# from selenium.webdriver.common.by import By
+#
+#
+# class LoginPage:
+#     def __init__(self, driver):
+#         self.driver = driver
+#         self.username_textbox = (By.ID, "username")
+#         self.password_textbox = (By.ID, "password")
+#         self.login_button = By.XPATH, "//button[normalize-space()='Login']"
+#
+#     def open_page(self, url):
+#         self.driver.get(url)
+#
+#     def enter_username(self, username):
+#         self.driver.find_element(*self.username_textbox).send_keys(username)
+#
+#     def enter_password(self, password):
+#         self.driver.find_element(*self.password_textbox).send_keys(password)
+#
+#     def click_login(self):
+#         self.driver.find_element(*self.login_button).click()
+
+# parameterized login page
 from selenium.webdriver.common.by import By
+import time
 
 
 class LoginPage:
@@ -20,3 +44,16 @@ class LoginPage:
 
     def click_login(self):
         self.driver.find_element(*self.login_button).click()
+
+    def get_alert_text_and_accept(self):
+        try:
+            alert=self.driver.switch_to.alert
+            alert_text=alert.text
+            alert.accept()
+            return alert_text
+        except:
+            return None
+
+    def is_dashboard_present(self):
+        time.sleep(2)
+        return "Welcome to the Dashboard" in self.driver.page_source
--- pom/run_test1.py
+++ pom/run_test1.py
@@ -18,15 +18,38 @@ def driver():
     # close the driver
     driver.quit()
 
-def test_login(driver):
-    login_page=LoginPage(driver)
-    login_page.open_page("https://tax.digitalpalika.org/login")
-    driver.maximize_window()
-    time.sleep(2)
-    login_page.enter_username("malikacounter5")
-    time.sleep(1)
-    login_page.enter_password("password")
-    time.sleep(1)
-    login_page.click_login()
-    time.sleep(1)
+# def test_login(driver):
+#     login_page=LoginPage(driver)
+#     login_page.open_page("https://tax.digitalpalika.org/login")
+#     driver.maximize_window()
+#     time.sleep(2)
+#     login_page.enter_username("malikacounter5")
+#     time.sleep(1)
+#     login_page.enter_password("password")
+#     time.sleep(1)
+#     login_page.click_login()
+#     time.sleep(1)
 
+
+@pytest.mark.parametrize("username,password",[
+    ("TestQA","password"),
+    ("A","password"),
+    ("1",""),
+])
+
+def test_login(driver, username, password):
+    login_page=LoginPage(driver)
+    login_page.open_page("https://sagar-test-qa.vercel.app/")
+    driver.maximize_window()
+    login_page.enter_username(username)
+    login_page.enter_password(password)
+    login_page.click_login()
+
+    alert_text=login_page.get_alert_text_and_accept()
+    if alert_text and "Invalid username or password" in alert_text:
+        print(f"Invalid username or password for {username}")
+    else:
+        if login_page.is_dashboard_present():
+            print(f"Login successful for {username}")
+        else:
+            print(f"Unexpected error or login failed for {username}")
